d987ad87bbbdc314eb647d401f1ef4e5cf52fa1a
Prajwol KC <prajwolpkc@outlook.com>
2024-11-05 11:01:48 +0545

    add traits folder

---
+++ app/Traits/GoogleSpeechRecognition.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Traits;
+
+use Google\Cloud\Speech\V1\SpeechClient;
+use Google\Cloud\Speech\V1\RecognitionAudio;
+use Google\Cloud\Speech\V1\RecognitionConfig;
+use Google\Cloud\Speech\V1\RecognitionConfig\AudioEncoding;
+
+trait GoogleSpeechRecognition
+{
+    public static function transcribe($wavFilePath)
+    {
+        $speechClient = new SpeechClient();
+
+        $audio = (new RecognitionAudio())
+            ->setContent(file_get_contents($wavFilePath));
+
+        $config = (new RecognitionConfig())
+            ->setEncoding(AudioEncoding::LINEAR16)
+            ->setSampleRateHertz(16000)
+            ->setLanguageCode('en-US');
+
+        $response = $speechClient->recognize($config, $audio);
+
+        $transcription = '';
+        foreach ($response->getResults() as $result) {
+            $transcription .= $result->getAlternatives()[0]->getTranscript();
+        }
+
+        return $transcription;
+    }
+
+}
